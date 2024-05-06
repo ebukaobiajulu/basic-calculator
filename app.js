a01114dba6d63fceb48a94df63098331c45984cf
Ebuka <93505445+ebukaobiajulu@users.noreply.github.com>
2024-05-06 09:27:55 +0100

    Updated changes to app.js

--- app.js
+++ app.js
@@ -7,22 +7,39 @@ header.style.fontSize = "3rem";
 let currentInput = "";
 let operator = "";
 let prevValue = null;
+let calculated = "";
 
+// for the number buttons
 function appendNumber(num) {
+  if (calculated) {
+    currentInput = "";
+    calculated = !calculated;
+  }
   currentInput += num;
   updateDisplay();
+  console.log(currentInput);
 }
 
+function appendDecimal() {
+  // Check if current input already contains a decimal point
+  if (!currentInput.includes(".")) {
+    currentInput += ".";
+    updateDisplay();
+  }
+}
+
+// mathematical operators
 function addOperator(op) {
   operator = op;
-  prevValue = parseFloat(currentInput);
+  prevValue = Number(currentInput);
   currentInput = "";
 }
 
+// Function to carry out the calculations
 function calculate() {
   if (operator && prevValue !== null && currentInput !== "") {
     let result;
-    const currentValue = parseFloat(currentInput);
+    const currentValue = Number(currentInput);
     switch (operator) {
       case "+":
         result = prevValue + currentValue;
@@ -39,18 +56,18 @@ function calculate() {
       case "/":
         result = prevValue / currentValue;
         break;
-      
     }
+
     currentInput = Number(result.toFixed(13)); //approximate to 13 decimal places
     operator = "";
     prevValue = null;
     updateDisplay();
-    currentInput = "";
-
-    
+    calculated = true;
+    // currentInput = '';
   }
 }
 
+// Update display in the input field
 function updateDisplay() {
   document.getElementById("display").value = Number(currentInput);
 }
